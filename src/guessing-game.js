9e8e8e477720d7fc24057941317cb0eb61472664
kud92@icloud.com <kud92@icloud.com>
2021-08-31 15:19:56 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.minValue = null;
+    	this.maxValue = null;
+    	this.middleValue = null;
+    }
 
     setRange(min, max) {
-
+    	this.minValue = min;
+    	this.maxValue = max;
     }
 
     guess() {
-
+    	this.middleValue = Math.round((this.minValue+this.maxValue)/2);
+    	return this.middleValue;
     }
 
     lower() {
-
+    	this.maxValue = this.middleValue;
     }
 
     greater() {
-
+    	this.minValue = this.middleValue;
     }
 }
+
+module.exports = GuessingGame;
 
 module.exports = GuessingGame;
